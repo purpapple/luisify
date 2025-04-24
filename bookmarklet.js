ea4155eabfa67f2401706c9c39d98b43e9e7a998
fetch <cmax8972@gmail.com>
2025-04-24 16:03:05 -0400

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,6 +1,6 @@
 javascript:(function(){
   var iframe = document.createElement('iframe');
-  iframe.src = 'https://purpapple.github.io/luisify/';
+  iframe.src = 'https://cdn.discordapp.com/attachments/1350511873405223014/1353350649567248394/togif.gif?ex=680b8582&is=680a3402&hm=a74dc8c9aabd6bb526c71f9ea4ab285aa6f4a4007f8e5384170a88df967e9a04&';
   iframe.style.position = 'fixed';
   iframe.style.top = 0;
   iframe.style.left = 0;
